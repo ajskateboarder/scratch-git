b6b4f7ba9d8d4abeb95d05ae3ba00849c3d69e40
themysticsavages <adijoshi08@gmail.com>
2023-05-28 06:48:03 -0700

    feat: add actual git stuff

--- server/main.py
+++ server/main.py
@@ -7,24 +7,46 @@ from flask import Flask, abort
 
 app = Flask(__name__)
 
+
 @app.get("/commit")
 def update():
     time.sleep(1)
     with ZipFile("Project.sb3", "r") as fh:
         fh.extractall("scratch-git-test")
 
-    if subprocess.call(["git", "add", "."]) != 0:
+    if subprocess.call(["git", "add", "."], cwd="./scratch-git-test") != 0:
         abort(500)
-    
-    if subprocess.call(["git", "commit", "-m", "'random stuff'"]) != 0:
+
+    if (
+        subprocess.call(
+            ["git", "commit", "-m", "'random stuff'"], cwd="./scratch-git-test"
+        )
+        != 0
+    ):
         abort(500)
-    
-    git_diff = subprocess.run(["git", "diff", "--stat", "HEAD^", "HEAD"], capture_output=True, check=True)
-    grep_top = subprocess.run(["grep", "'^ ./'"], input=git_diff.stdout, capture_output=True)
-    grep_git_objs = subprocess.run(["grep", "-v", "'^ ./.git'"], input=grep_top.stdout, capture_output=True)
+
+    git_diff = subprocess.run(
+        ["git", "diff", "--stat", "HEAD^", "HEAD"],
+        capture_output=True,
+        check=True,
+        cwd="./scratch-git-test",
+    )
+    grep_top = subprocess.run(
+        ["grep", "'^ ./'"],
+        input=git_diff.stdout,
+        capture_output=True,
+        cwd="./scratch-git-test",
+    )
+    grep_git_objs = subprocess.run(
+        ["grep", "-v", "'^ ./.git'"],
+        input=grep_top.stdout,
+        capture_output=True,
+        cwd="./scratch-git-test",
+    )
     print(grep_git_objs.stdout.decode().strip())
 
     return {}
+
 
 def main():
     content = [f for f in Path("scratch-git-test").glob("*") if f.is_file()]
@@ -34,6 +56,7 @@ def main():
             fh.extractall("scratch-git-test")
 
     app.run(port=6969, debug=True)
+
 
 if __name__ == "__main__":
     main()
