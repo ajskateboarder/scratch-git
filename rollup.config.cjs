c3197b35f7425c0fdf6342f988798ec5477d7869
Aditya J <81255936+ajskateboarder@users.noreply.github.com>
2024-02-10 19:41:12 +0000

    chore: update rollup entrypoint

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -1,7 +1,7 @@
 const { string } = require("rollup-plugin-string")
 
 module.exports = {
-    input: "src/main.js",
+    input: "src/frontend/main.js",
     output: { file: "userscript.js", format: "iife" },
     logLevel: "silent",
     plugins: [
