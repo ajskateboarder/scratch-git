a2ca1a626fbba6a7cb775c213a7d55983edf15d8
Aditya J <81255936+ajskateboarder@users.noreply.github.com>
2024-02-14 16:05:22 +0000

    chore: lol thought i added that script

---
+++ debug.py
@@ -0,0 +1,146 @@
+# Adapted from https://maxhalford.github.io/blog/flask-sse-no-deps/
+
+import queue
+
+from http.client import HTTPConnection
+from pathlib import Path
+from threading import Thread
+from platform import uname
+import shutil
+import os
+from os.path import expandvars
+from flask import Flask, Response, make_response, request
+
+
+class MessageAnnouncer:
+    def __init__(self):
+        self.listeners = []
+
+    def listen(self):
+        q = queue.Queue(maxsize=5)
+        self.listeners.append(q)
+        return q
+
+    def announce(self, msg):
+        for i in reversed(range(len(self.listeners))):
+            try:
+                self.listeners[i].put_nowait(msg)
+            except queue.Full:
+                del self.listeners[i]
+
+
+def cors_preflight():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Headers", "*")
+    response.headers.add("Access-Control-Allow-Methods", "*")
+    return response
+
+
+def cors(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
+
+def tw_path() -> Path | None:
+    platform = uname().system
+    if platform == "Windows":
+        pth = Path(expandvars("%APPDATA%")) / "turbowarp-desktop"
+        if list(pth.glob("*")):
+            return pth
+        pth = Path(expandvars("%LOCALAPPDATA%")) / "Packages"
+        try:
+            store_folder = next(
+                f.name for f in pth.glob("*") if "TurboWarpDesktop" in f.name
+            )
+            cache_exists = list((pth / store_folder).glob("LocalCache"))
+            if cache_exists:
+                return (
+                    pth / store_folder / "LocalCache" / "Roaming" / "turbowarp-desktop"
+                )
+        except StopIteration:
+            pass
+    if platform == "Linux":
+        HOME = expandvars("$HOME")
+        pth = Path(HOME) / ".config" / "turbowarp-desktop"
+        if list(pth.glob("*")):
+            return pth
+        pth = (
+            Path(HOME)
+            / ".var"
+            / "app"
+            / "org.turbowarp.TurboWarp"
+            / "config"
+            / "turbowarp-desktop"
+        )
+        if list(pth.glob("*")):
+            return pth
+        pth = (
+            Path(HOME)
+            / "snap"
+            / "turbowarp-desktop"
+            / "current"
+            / ".config"
+            / "turbowarp-desktop"
+        )
+        if list(pth.glob("*")):
+            return pth
+    if platform == "Darwin":
+        HOME = expandvars("$HOME")
+        pth = (
+            Path(HOME)
+            / "Library"
+            / "Application Support"
+            / "turbowarp-desktop"
+        )
+        if list(pth.glob("*")):
+            return pth
+        pth = (
+            Path(HOME)
+            / "Library"
+            / "Containers"
+            / "org.turbowarp.desktop"
+            / "Data"
+            / "Library"
+            / "Application Support"
+            / "turbowarp-desktop"
+        )
+        if list(pth.glob("*")):
+            return pth
+    return None
+
+
+app = Flask(__name__)
+announcer = MessageAnnouncer()
+
+
+@app.route("/update")
+def ping():
+    if request.method == "OPTIONS":
+        return cors_preflight()
+
+    announcer.announce(msg="data: update\n\n")
+    return {}
+
+
+@app.get("/listen")
+def listen():
+    if request.method == "OPTIONS":
+        return cors_preflight()
+
+    def stream():
+        messages = announcer.listen()
+        while True:
+            msg = messages.get()
+            yield msg
+
+    return Response(stream(), mimetype="text/event-stream")
+
+
+if __name__ == "__main__":
+    os.system("rollup -c")
+    shutil.copy2("userscript.js", tw_path())
+    Thread(target=lambda: app.run(port=3333)).start()
+    HTTPConnection("localhost", 3333).request(
+        "GET", "/update", headers={"Host": "localhost"}
+    )
