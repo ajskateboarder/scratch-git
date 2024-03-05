a86d1a9564ba1d0f341040c9e7de551ad2820afc
themysticsavages <adijoshi08@gmail.com>
2024-03-04 16:55:23 -0800

    chore: remove unused function

--- src/frontend/modals/welcome.js
+++ src/frontend/modals/welcome.js
@@ -177,24 +177,6 @@ export class WelcomeModal {
   }
 
   openProject() {
-    function waitForElm(selector) {
-      return new Promise((resolve) => {
-        if (document.querySelector(selector)) {
-          return resolve(document.querySelector(selector));
-        }
-        const observer = new MutationObserver((_) => {
-          if (document.querySelector(selector)) {
-            observer.disconnect();
-            resolve(document.querySelector(selector));
-          }
-        });
-        observer.observe(document.body, {
-          childList: true,
-          subtree: true,
-        });
-      });
-    }
-
     fileMenu.openProject();
     /** @type {HTMLButtonElement} */
     let nextButton = document.querySelector("#goToStep2");
