87ebe831a56803274c66fea1fc4607a452631221
Aditya J <81255936+ajskateboarder@users.noreply.github.com>
2024-03-14 10:56:08 -0400

    refactor: src folder is bad

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,12 @@
 const { string } = require("rollup-plugin-string");
 
 module.exports = {
-  input: "src/frontend/main.js",
+  input: "src-frontend/main.js",
   output: { file: "userscript.js", format: "iife" },
   logLevel: "silent",
   plugins: [
     string({
-      include: "src/frontend/media/*",
+      include: "src-frontend/media/*",
     }),
   ],
 };
