1ff85154ab623edb53d04bafbb3b5a8f6b82793a
themysticsavages <adijoshi08@gmail.com>
2024-05-10 19:18:01 -0700

    chore: better debugging

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,43 @@
+import { execFileSync, spawnSync } from "child_process";
+import { existsSync } from "fs";
+
 import typescript from "@rollup/plugin-typescript";
 import { string } from "rollup-plugin-string";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-// import terser from "@rollup/plugin-terser";
+import terser from "@rollup/plugin-terser";
+import copy from "rollup-plugin-copy";
 
-export default {
-  input: "src-frontend/index.ts",
-  output: { file: "userscript.js", format: "iife" },
-  logLevel: "silent",
-  plugins: [
-    string({
-      include: ["src-frontend/modals/thumbnail.svg", "src-frontend/styles.css"],
-    }),
-    nodeResolve(),
-    typescript(),
-    // TODO: apply terser in releases
-    // terser(),
-  ],
+export default async (args) => {
+  let build = {
+    input: "src-frontend/index.ts",
+    output: { file: "userscript.js", format: "iife" },
+    logLevel: "silent",
+    plugins: [
+      string({
+        include: [
+          "src-frontend/modals/thumbnail.svg",
+          "src-frontend/styles.css",
+        ],
+      }),
+      nodeResolve(),
+      typescript(),
+    ],
+  };
+
+  if (args.debug) {
+    if (!existsSync("tw_path.exe") && !existsSync("tw_path"))
+      spawnSync("rustc", ["src-server/tw_path.rs"], { encoding: "utf8" });
+    let path = (
+      existsSync("tw_path.exe")
+        ? execFileSync("./tw_path.exe", { encoding: "utf8" })
+        : execFileSync("./tw_path", { encoding: "utf8" })
+    ).trimEnd();
+    build.plugins.push(
+      copy({ targets: [{ src: "userscript.js", dest: path }] })
+    );
+  } else {
+    build.plugins.push(terser());
+  }
+
+  return build;
 };
