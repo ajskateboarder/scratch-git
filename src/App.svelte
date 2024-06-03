c46d9ab71fe0760c1decec168ecc9568e6fcf3f3
ajskateboarder <adijoshi08@gmail.com>
2024-06-03 13:54:03 -0700

    stuff

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,36 @@
+<script lang="ts">
+  import logo from "/logo.svg";
+  import lightDiff from "/light-diff.mp4";
+  import darkDiff from "/dark-diff.mp4";
+</script>
+
 <main>
-  <h1>Hello world!</h1>
+  <header>
+    <h1>
+      <img src={logo} alt="scratch.git logo" width="70" />
+      <span style="font-size: 40px;">scratch.git</span>
+    </h1>
+    <h2>
+      scratch.git provides a way to view/track your changes, revert to versions
+      changes of projects, and collaborate more efficiently from TurboWarp
+      Desktop.
+    </h2>
+  </header>
+  <div style="display: flex; flex-wrap: wrap">
+    <video autoplay loop muted>
+      <source src={lightDiff} />
+    </video>
+    <video autoplay loop muted height="276">
+      <source src={darkDiff} />
+    </video>
+  </div>
+  <footer>
+    <br />
+    scratch.git is not affiliated with Scratch, the Scratch Team, or the Scratch
+    Foundation.
+    <br />
+    <a href="https://github.com/ajskateboarder/scratch-git" target="_blank">
+      <i class="fas sack" /></a
+    >
+  </footer>
 </main>
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
-const replaceAssetsPaths = () => ({
-  name: "replace-assets-paths",
-  transformIndexHtml: {
-    handler(html) {
-      return html.replace(/\/assets/gi, "assets");
-    },
-  },
-});
-
 export default defineConfig({
-  plugins: [svelte(), replaceAssetsPaths()],
+  plugins: [svelte()],
+  base: "",
+  publicDir: "public",
 });
