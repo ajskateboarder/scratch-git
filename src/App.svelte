baf6eac98e9749d6f3cde16b266015a8e959e7eb
ajskateboarder <adijoshi08@gmail.com>
2024-06-03 18:44:03 -0700

    stuff

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,72 @@
 <script lang="ts">
+  import Project from "./Project.svelte";
+
   import logo from "/logo.svg";
   import lightDiff from "/light-diff.mp4";
   import darkDiff from "/dark-diff.mp4";
+  import { onMount } from "svelte";
+
+  let lightDiffVideo: HTMLVideoElement;
+  let darkDiffVideo: HTMLVideoElement;
+  let hash = "";
+
+  const playOnHover = (video: HTMLVideoElement) => {
+    Object.assign(video, {
+      onmouseover: () => video.play(),
+      onmouseleave: () => video.pause(),
+    });
+  };
+
+  const routeChange = () => {
+    let url = window.location.hash.substring(1);
+    if (url === "") {
+      hash = "";
+    }
+    try {
+      new URL(url);
+      hash = url;
+    } catch {
+      console.warn("bad url");
+    }
+  };
+
+  onMount(() => {
+    playOnHover(lightDiffVideo);
+    playOnHover(darkDiffVideo);
+
+    routeChange();
+  });
 </script>
 
-<main>
-  <header>
-    <h1>
-      <img src={logo} alt="scratch.git logo" width="70" />
-      <span style="font-size: 40px;">scratch.git</span>
-    </h1>
-    <h2>
-      scratch.git provides a way to view/track your changes, revert to versions
-      changes of projects, and collaborate more efficiently from TurboWarp
-      Desktop.
-    </h2>
-  </header>
+<svelte:window on:hashchange={routeChange} />
+
+<header>
+  <h1>
+    <img src={logo} alt="scratch.git logo" width="70" />
+    <span style="font-size: 40px;">scratch.git</span>
+  </h1>
+  <p>
+    scratch.git provides a way to view/track your project's changes, revert to
+    previous versions, and collaborate more efficiently from TurboWarp Desktop.
+  </p>
+</header>
+<Project url={hash}></Project>
+<details>
+  <summary class="kinda-big"> <span>Diffing</span> </summary>
   <div style="display: flex; flex-wrap: wrap">
-    <video autoplay loop muted>
+    <video loop muted height="230" bind:this={lightDiffVideo}>
       <source src={lightDiff} />
     </video>
-    <video autoplay loop muted height="276">
+    <video loop muted height="230" bind:this={darkDiffVideo}>
       <source src={darkDiff} />
     </video>
   </div>
-  <footer>
-    <br />
-    scratch.git is not affiliated with Scratch, the Scratch Team, or the Scratch
-    Foundation.
-    <br />
-    <a href="https://github.com/ajskateboarder/scratch-git" target="_blank">
-      <i class="fas sack" /></a
-    >
-  </footer>
-</main>
+</details>
+<footer>
+  <br />
+  scratch.git is not affiliated with Scratch, the Scratch Team, or the Scratch Foundation.
+  <br />
+  <a href="https://github.com/ajskateboarder/scratch-git" target="_blank"
+    >source code</a
+  >
+</footer>
---
+++ src/Project.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let url: string;
+</script>
+
+<main>
+  {#if url !== ""}
+    cool
+  {/if}
+  <input
+    type="text"
+    class="project-input"
+    placeholder="https://somerepo.com/user/repository"
+  />
+  {url}
+</main>
