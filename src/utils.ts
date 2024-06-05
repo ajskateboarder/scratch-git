d1bb0cca4e8577c63e1e7c8a183bb26c5926954c
Aditya J <81255936+ajskateboarder@users.noreply.github.com>
2024-06-05 17:29:12 +0000

    fix: priority of cors proxy

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,13 @@
 export const proxy = async (url: string) => {
   return fetch(
-    `https://universal-cors-proxy.glitch.me/${encodeURIComponent(url)}`,
+    `https://api.codetabs.com/v1/proxy?quest=${encodeURIComponent(url)}`
   )
     .then((e) => {
       return e;
     })
     .catch(async () => {
       const request = await fetch(
-        `https://api.codetabs.com/v1/proxy?quest=${encodeURIComponent(url)}`,
+        `https://universal-cors-proxy.glitch.me/${encodeURIComponent(url)}`,
       );
 
       assert(request.ok, "Failed to make request to: " + url);
